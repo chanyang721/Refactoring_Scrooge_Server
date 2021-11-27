58e212400bb9b84aef654595398aae6c571fbd80
chanyang217 <chanyang721@gmail.com>
2021-11-28 00:03:13 +0900

    [Edit] Jwt decode logic

--- src/helper/utils/jwt.ts
+++ src/helper/utils/jwt.ts
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
 import { Request, Response } from "express";
-import { sign, verify, Algorithm, SignOptions, JwtPayload } from "jsonwebtoken";
+import { sign, verify, Algorithm, SignOptions, VerifyOptions, JwtPayload } from "jsonwebtoken";
 import { Service } from "typedi";
 import config from "../../config"
 dotenv.config();
@@ -10,51 +10,41 @@ export default class Jwt {
 
     public tokenGenerator = (subject: string, expiresIn: string) => {
         const algorithm = config.jwt.algorithm as Algorithm;
-        const JwtOptions: SignOptions = { algorithm, expiresIn, subject }
+        const jwtOptions: SignOptions = { algorithm, expiresIn, subject }
 
         if(subject === "ACCESSS_TOKEN") {
             return ({ idx, id }: JwtPayload) => 
-                sign({ idx, id }, config.jwt.secret, JwtOptions);
+                sign({ idx, id }, config.jwt.secret, jwtOptions);
         }
         if(subject === "REFRESH_TOKEN") {
             return ({ id }: JwtPayload) =>
-                sign({ id }, config.jwt.secret, JwtOptions);
+                sign({ id }, config.jwt.secret, jwtOptions);
         }
 
-        return () => sign({}, config.jwt.secret, JwtOptions);
+        return () => sign({}, config.jwt.secret, jwtOptions);
     }
-
-    // public generateAccessToken = (data: object) => {
-    //     return sign(data, process.env.ACCESS_SECRET, { expiresIn: "10h" });
+    // public sendToken = (res: Response, accessToken: string, refreshToken: string) => {
+    //     return res
+    //         .status(200)
+    //         .cookie("refreshToken", refreshToken, {
+    //             sameSite: "none",
+    //             secure: true,
+    //             httpOnly: true,
+    //         })
+    //         .send({ data: { accessToken, refreshToken }, message: "로그인 완료" });
     // }
 
-    // public generateRefreshToken = (data: object) => {
-    //     return sign(data, process.env.REFRESH_SECRET, { expiresIn: "90d" });
+    // public resendAccessToken = (res: Response, accessToken: string, data: string) => {
+    //     return res.send({
+    //         data: { accessToken, user: data },
+    //         message: "accessToken 재발급 완료",
+    //     });
     // }
 
-    public sendToken = (res: Response, accessToken: string, refreshToken: string) => {
-        return res
-            .status(200)
-            .cookie("refreshToken", refreshToken, {
-                sameSite: "none",
-                secure: true,
-                httpOnly: true,
-            })
-            .send({ data: { accessToken, refreshToken }, message: "로그인 완료" });
-    }
-
-    public resendAccessToken = (res: Response, accessToken: string, data: string) => {
-        return res.send({
-            data: { accessToken, user: data },
-            message: "accessToken 재발급 완료",
-        });
-    }
-
-    public checkToken = (token: string, secret: string) => {
-        try {
-            return verify(token, secret);
-        } catch (err) {
-            return null;
-        }
+    public decodeToken = (subject: string, token: string) => {
+        const algorithm = config.jwt.algorithm as Algorithm;
+        const jwtOptions: VerifyOptions = { algorithms: [algorithm], subject }
+        
+        return verify(token, config.jwt.secret, jwtOptions);
     }
 }
